4b63081c38efe989e76f7dd86fb6f959e606077c
PKPakpoom <pai38985@gmail.com>
2026-04-27 20:08:12 +0700

    feat: include user role in JWT token payload
    - Add role field to JWT token generation for role-based authentication
    - Enables frontend to properly redirect users based on their role

--- models/User.js
+++ models/User.js
@@ -76,7 +76,7 @@ UserSchema.pre('save', async function(next){
 });
 
 UserSchema.methods.getSignedJwtToken = function() {
-    return jwt.sign({id:this._id}, process.env.JWT_SECRET, {
+    return jwt.sign({id:this._id, role:this.role}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
 }
